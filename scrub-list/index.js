9aef4c44a3d2aa3ff7c6cc78c05b555f5cfc0c1c
Patrick Browne <pt.browne@gmail.com>
2018-08-27 11:54:27 +0200

    refactor: perf of the scrub bar

--- scrub-list/index.js
+++ scrub-list/index.js
@@ -30,14 +30,27 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-between',
     touchAction: 'none',
-    cursor: 'move'
+    cursor: 'pointer'
   },
-  ScrubberElements: {
+  ScrubItem: {
+    height: 20,
+    fontSize: '0.75rem'
+  },
+  ScrubItems: {
     position: 'relative',
     height: '100%',
     display: 'flex',
     justifyContent: 'space-between',
     flexDirection: 'column'
+  },
+  ScrubBar: {
+    position: 'absolute',
+    cursor: 'move',
+    zIndex: 0,
+    width: '100%',
+    // transition: 'transform 0.125s ease',
+    height: 50,
+    background: '#eee'
   },
   LightPokeAvatar: {
     display: 'inline-block',
@@ -80,16 +93,22 @@ class WithDimensions extends React.Component {
     this.update = this.update.bind(this)
   }
   componentDidMount () {
-    console.log('node', this.node)
     window.addEventListener('resize', this.update)
     this.update()
+    setTimeout(() => {
+      this.update()
+    }, 1000)
   }
   componentWillUnmount() {
     window.removeEventListener('resize', this.update)
   }
   update() {
+    const { height } = this.node.getBoundingClientRect()
     this.setState({
-      dimensions: this.node.getBoundingClientRect()
+      dimensions: {
+        bodyHeight: document.body.offsetHeight,
+        height: height
+      }
     })
   }
   render () {
@@ -103,63 +122,92 @@ class WithDimensions extends React.Component {
   }
 }
 
-class Scrubber extends React.Component {
-  constructor() {
+class ScrubBar extends React.PureComponent {
+  constructor () {
     super()
+    this.state = {}
     this.handleMouseMove = this.handleMouseMove.bind(this)
-    this.handleMouseDown = this.handleMouseDown.bind(this)
-    this.handleMouseUp = this.handleMouseUp.bind(this)
-    this.handleTouchMove = throttle(this.handleTouchMove.bind(this), 100, { leading: true, trailing: true })
-    document.addEventListener('mousedown', this.handleMouseDown)
-    document.addEventListener('mouseup', this.handleMouseUp)
   }
 
-  handleMouseDown (ev) {
-    if (ev.which === 1) { this.clicking = true }
+  componentDidMount () {
+    document.addEventListener('mousemove', this.handleMouseMove)
   }
 
-  handleMouseUp (ev) {
-    if (ev.which === 1) { this.clicking = false }
+  componentWillUnmount () {
+    document.removeEventListener('mousemove', this.handleMouseMove)
   }
 
-  handleMouseMove(ev, i) {
-    ev.preventDefault()
-    if (this.clicking) {
-      this.props.onScrub(i)
-    }
-  }
-
-  handleTouchMove (ev, height) {
-    ev.preventDefault()
-    if (!ev.touches) {
+  handleMouseMove (ev) {
+    if (!this.props.clicking) {
       return
     }
-    const y = ev.touches[0].pageY - window.scrollY
+    const { height, list } = this.props
+    const y = (ev.touches ? ev.touches[0].pageY : ev.pageY) - window.scrollY
     const perc = y / height
-    const list = this.props.list
-    this.props.onScrub(Math.round(perc * list.length))
-    this.setState({ i: Math.round(perc * list.length) })
+    this.props.onScrub(perc)
   }
 
   render () {
-    const list = this.props.list
-    const top = `translateY(${this.props.scrollTop / (styles.Row.height * list.length) * 100}vh)`
+    const { perc, height } = this.props
+    const ty = `translateY(${perc * height}px)`
+    return (
+      <div
+        style={Object.assign({
+           transform: ty
+        }, styles.ScrubBar)} />
+    )
+  }
+}
+
+class Scrubber extends React.Component {
+  constructor() {
+    super()
+    this.handleMouseDown = this.handleMouseDown.bind(this)
+    this.handleMouseUp = this.handleMouseUp.bind(this)
+    this.onScrub = throttle(this.onScrub.bind(this), 16)
+    this.state = { clicking: false }
+  }
+
+  componentDidMount() {
+    document.addEventListener('mouseup', this.handleMouseUp)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mouseup', this.handleMouseUp)
+  }
+
+  handleMouseDown (ev) {
+    this.setState({ clicking: true })
+  }
+
+  handleMouseUp (ev) {
+    this.setState({ clicking: false })
+  }
+
+  onScrub (perc) {
+    this.props.onScrub(perc)
+  }
+
+  render () {
+    const { clicking } = this.state
+    const { list, scrollTop } = this.props
     return (
       <WithDimensions style={styles.Scrubber}>{
         dimensions => {
-          const scrubLineHeight = 20
+          const scrubLineHeight = styles.ScrubItem.height
           const elements = list.length
           const nbElements = Math.round(dimensions.height / scrubLineHeight) - 1
           const steps = Math.round(elements / nbElements)
           const numbers = getBoundaries(list, row => Math.round(parseInt(row.number) / steps))
-          return <div onTouchMove={ev => { ev.persist(); this.handleTouchMove(ev, dimensions.height)}}>
-            <div style={{ position: 'absolute', zIndex: 0, width: '100%', transition: 'transform 0.125s ease', transform: top, height: 20, background: '#eee' }}></div>
-            <div style={styles.ScrubberElements}>{
+          const perc = scrollTop / dimensions.bodyHeight
+          return <div>
+            <ScrubBar perc={perc} clicking={clicking} onScrub={this.onScrub} list={list} height={dimensions.height} />
+            <div onMouseDown={this.handleMouseDown} style={styles.ScrubItems}>{
               numbers.map(([i, number]) => {
                 const pokemon = pokemons[i]
                 return (
                   <div
-                    onMouseMove={(ev) => this.handleMouseMove(ev, i)}
+                    key={i}
                     style={styles.ScrubItem}
                     onClick={() => this.props.onScrub(i)}
                   >
@@ -216,12 +264,12 @@ class App extends React.PureComponent {
     );
   }
 
-  _scrollTo (index) {
-    setTimeout(() => {
-      this.setState({
-        scrollToIndex: index
-      })
-    }, 100)
+  _scrollTo (percentage) {
+    const { list } = this.props
+    const index = Math.round(percentage * list.length)
+    this.setState({
+      scrollToIndex: index
+    })
   }
 
   render () {
@@ -233,7 +281,7 @@ class App extends React.PureComponent {
         scrollElement={window}>
         {({height, isScrolling, registerChild, onChildScroll, scrollTop}) => (
           <div>
-            <Scrubber list={list} onScrub={this._scrollTo} scrollTop={scrollTop} height={height}/>
+            <Scrubber list={list} scrollTop={scrollTop} onScrub={this._scrollTo}/>
             <AutoSizer disableHeight>
               {({width}) => (
                 <div ref={registerChild}>
